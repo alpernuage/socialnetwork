bfca24efcf3914aa88a03dd40174f7f8c1bf3d70
Alper <alperakbulut@gmail.com>
2020-05-27 13:56:59 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,22 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $preparedRequest = $PDO->prepare("select * from user where pseudo=:nickname and mdp=:password");
+  $preparedRequest->execute(
+    array(
+      "nickname" => $username,
+      "password" => $password
+    )
+  );
+  $users = $preparedRequest->fetchAll();
+  if (count($users) == 1) {
+    $user = $users[0];
+    return $user['id'];
+  } else {
+    return -1;
+  }
+}
